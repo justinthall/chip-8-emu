5aebc62329b15562283bfc9d030bae6fb805af05
Khajeet/Justin <justin.thall99@gmail.com>
2020-10-02 23:39:12 -0400

    opcode 8 cases

--- opcodes.cpp
+++ opcodes.cpp
@@ -52,15 +52,65 @@ switch(opcode&0xF000){
 		}
 		break;
 	case 0x6000:
-		v_registers[(opcode&0x0F00)>>8] = (opcode&0x00FF)
+		v_registers[(opcode&0x0F00)>>8] = (opcode&0x00FF);
 		program_counter += 2;
 		break;
 	case 0x7000:
-		v_registers[(opcode&0x0F00)>>8] = (opcode&0x00FF)
+		v_registers[(opcode&0x0F00)>>8] = (opcode&0x00FF);
 		program_counter += 2;
 		break;
 	case 0x8000:
-		switch(opcode&0x000F)
+		switch(opcode&0x000F){
+			case 0x0000:
+				v_registers[(opcode&0x0F00)>>8] = v_registers[(opcode&0x00F0)>>4];
+				program_counter += 2;
+				break;
+			case 0x0001:
+				v_registers[(opcode&0x0F00)>> 8] = (v_registers[(opcode&0x0F00)>> 8]|v_registers[(opcode&0x00F0)>>4]);
+				program_counter += 2;
+				break;
+			case 0x0002:
+				v_registers[(opcode&0x0F00)>> 8] = (v_registers[(opcode&0x0F00)>> 8]&v_registers[(opcode&0x00F0)>>4]);
+				program_counter += 2;
+				break;
+			case 0x0003:
+				v_registers[(opcode&0x0F00)>> 8] = (v_registers[(opcode&0x0F00)>> 8]^v_registers[(opcode&0x00F0)>>4]);
+				program_counter += 2;
+				break;
+			case 0x0004:
+				if(v_registers[(opcode & 0x00F0) >> 4] > (0xFF - v_registers[(opcode & 0x0F00) >> 8])){ 
+						v_registers[0xF] = 1;}
+				else{ 
+						v_registers[0xF] = 0;}					
+				v_registers[(opcode & 0x0F00) >> 8] += v_registers[(opcode & 0x00F0) >> 4];
+				program_counter += 2;
+				break;
+			case 0x0005:
+				if(v_registers[(opcode&0x00F0)>>4] > v_registers[(opcode&0x0F00)>>8]){
+				v_registers[0xF] = 1;
+				}
+				else{
+					v_registers[0xF] = 0;
+				}
+				v_registers[(opcode & 0x0F00) >> 8] -= v_registers[(opcode & 0x00F0) >> 4];
+				program_counter += 2;
+				break;
+			case 0x0006:
+				v_registers[0xF] = v_registers[(opcode&0x0F00)>>8] & 0x1;
+				v_registers[(opcode&0x0F00)>>8]>>= 1;
+				program_counter += 2;
+				break;
+			case 0x0007:
+				if(v_registers[(opcode&0x0F00)>>8] > v_registers[(opcode&0x00F0)>>4]){
+				v_registers[0xF] = 0;
+				}
+				else{
+					v_registers[0xF] = 1;
+				}
+				v_registers[(opcode & 0x00F0) >> 4] -= v_registers[(opcode & 0x0F00) >> 8];
+				program_counter += 2;
+				break;
+		}
 
 	default:
 	printf("Unknown opcode: 0x%X", opcode)
